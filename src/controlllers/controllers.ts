a6095919318f0001024b14555089021653c31a36
PeytonDenisse <dcv78658@lasalle.edu.mx>
2024-10-13 16:45:21 -0600

    agregue el put de actualizar tarea

--- src/controlllers/controllers.ts
+++ src/controlllers/controllers.ts
@@ -36,3 +36,15 @@ export const createTarea = async (req: Request, res: Response) => {
         res.status(500).json({ message: "Tarea no creada"});
     }
 };
+
+//put actualizar una tarea
+export const updateTarea = async (req: Request, res: Response) => {
+    try {
+        const tarea = await Tarea.findByPk(req.params.id);
+        if (!tarea) return res.status(404).json({ message: 'Tarea no encontrada' });
+        await tarea.update(req.body);
+        res.json(tarea);
+    } catch (error) {
+        res.status(500)
+    }
+};
